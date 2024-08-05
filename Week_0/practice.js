38261f9314948000974824a37a4f88b33748610b
git-rafaykhan <rafaykhan2k19@gmail.com>
2024-08-05 00:44:57 -0700

    Added week 0 practice.js and its Readme.md file

--- Week_0/practice.js
+++ Week_0/practice.js
@@ -1,6 +1,26 @@
 const crypto = require('crypto');
 
-const input = "100xdevs";
-const hash = crypto.createHash('sha256').update(input).digest('hex');
+// You have to install this package  =>   npm i cyrpto 
+// Function to find a hash that starts with a given prefix
+// It iterates through numbers to find the nonce (the number that generates the required hash)
 
-console.log(hash);
+function findHashStartsWithPrefix(prefix) {
+    let input = 0;
+    while (true) {
+        // Create a string with the current input
+        let inpStr = `Rafay => wasay | Rs 40 ${input}`;
+        
+        // Generate SHA-256 hash of the input string
+        const hash = crypto.createHash('sha256').update(inpStr).digest('hex');
+        
+        // Check if the hash starts with the specified prefix
+        if (hash.startsWith(prefix)) {
+            return { input: inpStr, hash: hash };
+        }
+        input++;
+    }
+}
+
+// Find a hash that starts with "0000"
+let data = findHashStartsWithPrefix("0000");
+console.log(`Input: ${data.input}, Hash: ${data.hash}`);
